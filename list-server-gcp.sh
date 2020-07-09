a277efef4e07fc53cc51dc89d3565753a6cd166f
hshmtkzhr <hshmtkzhr@gmail.com>
2020-07-10 01:21:05 +0900

    revise expression of python part of this script.

--- list-server-gcp.sh
+++ list-server-gcp.sh
@@ -36,19 +36,25 @@ import sys, json, os
 
 j = json.load(sys.stdin, encoding="utf-8")
 for elm in j:
-  name = elm["name"]
-  status = elm["status"]
-  ip = elm["networkInterfaces"][0]["networkIP"]
-  created_at = elm["creationTimestamp"]
-  machine_type = os.path.basename(elm["machineType"]).rstrip()
-  zone = os.path.basename(elm["zone"]).rstrip()
-  tags = ",".join(elm["tags"]["items"])
+  name   = elm.get("name", "")
+  status = elm.get("status", "")
+  ip     = elm.get("networkInterfaces", [{}])[0].get("networkIP", "")
+  #
+  created_at   = elm.get("creationTimestamp", "")
+  machine_type = os.path.basename(elm.get("machineType", "")).rstrip()
+  #
+  zone = os.path.basename(elm.get("zone", "")).rstrip()
+  tags = ",".join(elm.get("tags", {}).get("items", ""))
+  #
   hostname = ""
   for i in elm["metadata"]["items"]:
     if i["key"] == "Hostname":
       hostname = i["value"]
 
-  print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(name, hostname, machine_type, status, zone, ip, tags, created_at))
+  print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(
+    name, hostname, machine_type, status, zone, ip, tags, created_at
+    )
+  )
 ' > $tmp.list
 
 cat $tmp.list | sort -k1,5 > $tmp.sorted
